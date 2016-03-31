030c8602d36b639192c45947e65662740753e944
James Nebeker <jnebeker@radiusnetworks.com>
2016-03-31 15:45:52 -0400

    Browserify and update Docs
    Use the gulp task to browserify the new code changes and compile them
    into one JS file: 'eddystone-advertising.js'.  Also generate new docs
    based on iBeacon changes.

--- libraries/javascript/eddystone-advertising/eddystone-advertising.js
+++ libraries/javascript/eddystone-advertising/eddystone-advertising.js
@@ -22,7 +22,8 @@
   const EddystoneFrameType = {
     URL: 'url',
     UID: 'uid',
-    TLM: 'tlm'
+    TLM: 'tlm',
+    IBEACON: 'ibeacon'
   };
 
   /**
@@ -33,6 +34,15 @@
      @alias module:eddystone-advertisement.EDDYSTONE_UUID
    */
   const EDDYSTONE_UUID = 'FEAA';
+
+  /**
+     Assigned ID for iBeacon Advertisements
+     @private
+     @constant {string}
+     @default
+     @alias module:eddystone-advertisement.IBEACON_ID
+   */
+  const IBEACON_ID = '004C';
 
   /**
      Represents the Advertisement being broadcasted.
@@ -65,8 +75,8 @@
        */
       this.type = undefined;
       /**
-         Tx Power included in the advertisement. Only present if `type === 'url'`
-         or `type === 'uid'`.
+         Tx Power included in the advertisement. Only present if `type === 'url'`,
+         `type === 'uid'`, or `type === 'ibeacon'`.
          @type {number|undefined}
        */
       this.advertisedTxPower = undefined;
@@ -85,6 +95,21 @@
          @type {string|undefined}
       */
       this.instance = undefined;
+      /**
+         Hex string of the uuid being advertised. Only present if `type === 'ibeacon'`.
+         @type {string|undefined}
+      */
+      this.uuid = undefined;
+      /**
+         Hex string of the major being advertised. Only present if `type === 'ibeacon'`.
+         @type {string|undefined}
+      */
+      this.major = undefined;
+      /**
+         Hex string of the uuid being advertised. Only present if `type === 'ibeacon'`.
+         @type {string|undefined}
+      */
+      this.minor = undefined;
 
       if (options.type == EddystoneFrameType.URL) {
         this.id = id;
@@ -97,6 +122,13 @@
         this.advertisedTxPower = options.advertisedTxPower;
         this.namespace = options.namespace;
         this.instance = options.instance;
+      } else if (options.type == EddystoneFrameType.IBEACON) {
+        this.id = id;
+        this.type = options.type;
+        this.advertisedTxPower = options.advertisedTxPower;
+        this.uuid = options.uuid;
+        this.major = options.major;
+        this.minor = options.minor;
       } else {
         throw new Error('Unsupported Frame Type');
       }
@@ -118,6 +150,7 @@
   exports.EddystoneAdvertisement = EddystoneAdvertisement;
   exports.EddystoneFrameType = EddystoneFrameType;
   exports.EDDYSTONE_UUID = EDDYSTONE_UUID;
+  exports.IBEACON_ID = IBEACON_ID;
 })();
 
 },{}],2:[function(require,module,exports){
@@ -197,6 +230,8 @@
         Eddystone._checkURLOptions(options);
       } else if (options.type === EddystoneFrameType.UID) {
         Eddystone._checkUIDOptions(options);
+      } else if (options.type === EddystoneFrameType.IBEACON) {
+        Eddystone._checkIBEACONOptions(options);
       } else {
         throw new TypeError('Unsupported Frame Type: ' + options.type);
       }
@@ -228,12 +263,26 @@
         throw new TypeError('Required member instance is undefined.');
       }
     }
+    static _checkIBEACONOptions(options) {
+      if (!('advertisedTxPower' in options)) {
+        throw new TypeError('Required member advertisedTxPower is undefined.');
+      }
+      if (!('uuid' in options)) {
+        throw new TypeError('Required member uuid is undefined.');
+      }
+      if (!('major' in options)) {
+        throw new TypeError('Required member major is undefined.');
+      }
+      if (!('minor' in options)) {
+        throw new TypeError('Required member minor is undefined.');
+      }
+    }
   }
 
   module.exports = Eddystone;
 })();
 
-},{"./eddystone-advertisement.js":1,"./platform.js":7}],3:[function(require,module,exports){
+},{"./eddystone-advertisement.js":1,"./platform.js":8}],3:[function(require,module,exports){
 (() => {
   'use strict';
 
@@ -244,9 +293,11 @@
 
   let EddystoneURL = require('./eddystone-url.js');
   let EddystoneUID = require('./eddystone-uid.js');
+  let IBeacon = require('./ibeacon.js');
   let EddystoneAdvertisement = require('./eddystone-advertisement.js').EddystoneAdvertisement;
   const EddystoneFrameType = require('./eddystone-advertisement.js').EddystoneFrameType;
   const EDDYSTONE_UUID = require('./eddystone-advertisement.js').EDDYSTONE_UUID;
+  const IBEACON_ID = require('./eddystone-advertisement.js').IBEACON_ID;
   /**
      This class wraps the underlying ChromeOS BLE Advertising API.
      @todo Add link to API.
@@ -260,6 +311,7 @@
        @property {string} type
        @property {Array} serviceUuids
        @property {Object} serviceData {uuid: {string}, data: {number[]}}
+       @property {Object} manufacturerData {id: {int}, data: {number[]}}
      */
 
     /**
@@ -321,29 +373,41 @@
        https://github.com/google/eddystone/tree/master/eddystone-url#eddystone-url-http-url-encoding
      */
     static _constructAdvertisement(options) {
-      let data;
-      if (options.type === EddystoneFrameType.URL) {
-        data = EddystoneURL.constructServiceData(options.url, options.advertisedTxPower);
-      } else if (options.type === EddystoneFrameType.UID) {
-        data = EddystoneUID.constructServiceData(
-          options.advertisedTxPower, options.namespace, options.instance);
+      let data, advertisement;
+      if (options.type ===  EddystoneFrameType.URL || options.type === EddystoneFrameType.UID) {
+        if (options.type === EddystoneFrameType.URL) {
+          data = EddystoneURL.constructServiceData(options.url, options.advertisedTxPower);
+        } else if (options.type === EddystoneFrameType.UID) {
+          data = EddystoneUID.constructServiceData(
+            options.advertisedTxPower, options.namespace, options.instance);
+        }
+        advertisement = {
+          type: 'broadcast',
+          serviceUuids: [EDDYSTONE_UUID],
+          serviceData: [{
+            uuid: EDDYSTONE_UUID,
+            data: data
+          }]
+        };
+      } else if (options.type ===  EddystoneFrameType.IBEACON) {
+        data = IBeacon.constructManufacturerData(options.uuid, options.major, options.minor, options.advertisedTxPower);
+        advertisement = {
+          type: 'broadcast',
+          manufacturerData: [{
+            id: IBEACON_ID,
+            data: data
+          }]
+        };
       } else {
         throw new Error('Unsupported Frame Type: ' + options.type);
       }
-      return {
-        type: 'broadcast',
-        serviceUuids: [EDDYSTONE_UUID],
-        serviceData: [{
-          uuid: EDDYSTONE_UUID,
-          data: data
-        }]
-      };
+      return advertisement;
     }
   }
   module.exports = EddystoneChromeOS;
 })();
 
-},{"./eddystone-advertisement.js":1,"./eddystone-uid.js":4,"./eddystone-url.js":5}],4:[function(require,module,exports){
+},{"./eddystone-advertisement.js":1,"./eddystone-uid.js":4,"./eddystone-url.js":5,"./ibeacon.js":7}],4:[function(require,module,exports){
 (() => {
   'use strict';
 
@@ -677,6 +741,155 @@
 
 }).call(this,typeof global !== "undefined" ? global : typeof self !== "undefined" ? self : typeof window !== "undefined" ? window : {})
 },{"./eddystone-advertising.js":2}],7:[function(require,module,exports){
+(() => {
+  'use strict';
+
+  /**
+   * @module ibeacon
+   * @typicalname ibeacon
+   */
+
+  const IBEACON_ID = require('./eddystone-advertisement.js').IBEACON_ID;
+
+  /**
+     iBeacon Frame type.
+     @private
+     @constant {number}
+     @default
+   */
+  const IBEACON_PREAMBLE = 0x0215;
+
+  const UUID_LENGTH = 16;
+  const MAJOR_MINOR_LENGTH = 2;
+
+  const HEX_REGEX = /[0-9a-f]/i;
+
+  /**
+     This class provides helper functions that relate to iBeacon.
+     @alias module:ibeacon
+   */
+  class IBeacon {
+    /**
+       Constructs a valid iBeacon manufacturer data from a Tx Power value, uuid,
+       major and minor.
+       @param {number} advertisedTxPower The Tx Power included in the service data.
+       @param {number[]|string} uuid The uuid to advertise.
+       @param {number[]|number} major The major to advertise.
+       @param {number[]|number} minor The minor to advertise.
+       @returns {number[]} The manufacturer data.
+     */
+    static constructManufacturerData(uuid, major, minor, advertisedTxPower) {
+      // Check that it's a valid Tx Power.
+      if (advertisedTxPower < -100 || advertisedTxPower > 20) {
+        throw new Error('Invalid Tx Power value: ' + advertisedTxPower + '.');
+      }
+      let base_frame = [IBEACON_PREAMBLE];
+      Array.prototype.push.apply(base_frame, IBeacon._getUuidByteArray(uuid));
+      Array.prototype.push.apply(base_frame, IBeacon._getMajorMinorByteArray(major));
+      Array.prototype.push.apply(base_frame, IBeacon._getMajorMinorByteArray(minor));
+      Array.prototype.push.apply(base_frame, [advertisedTxPower]);
+      return base_frame;
+    }
+
+    /**
+       Validates the given array of bytes or converts the hex string into an array of bytes.
+       @param {number[]|string} value The value to encode.
+       @throws {TypeError} If |value| is not an array or a string.
+       @throws {Error} If |value| contains out-of-range numbers or characters.
+       @returns {number[]} Array of bytes.
+    */
+    static getByteArray(value, expected_length) {
+      if (typeof value === 'string') {
+        // A hex string is twice as long as the byte array it represents.
+        let str_expected_length = expected_length * 2;
+        return IBeacon._encodeString(value, str_expected_length);
+      }
+      if (typeof value === 'number') {
+        return IBeacon._encodeNumber(value, expected_length);
+      }
+      if (Array.isArray(value)) {
+        return IBeacon._validateByteArray(value, expected_length);
+      }
+      throw new TypeError('Only string or array are supported');
+    }
+
+    static getHexString(bytes) {
+      let hex_string = '';
+      for (let i = 0; i < bytes.length; i++) {
+        if (bytes[i] > 0xFF) {
+          throw new Error('Invalid value \'' + bytes[i] + '\' at index ' + i + '.');
+        }
+        hex_string = hex_string.concat((bytes[i] >>> 4).toString(16));
+        hex_string = hex_string.concat((bytes[i] & 0xF).toString(16));
+      }
+      return hex_string;
+    }
+
+    static _getUuidByteArray(uuid) {
+      return IBeacon.getByteArray(uuid, UUID_LENGTH);
+    }
+
+    static _getMajorMinorByteArray(majorMinor) {
+      return IBeacon.getByteArray(majorMinor, MAJOR_MINOR_LENGTH);
+    }
+
+    static _encodeString(str, expected_length) {
+      if (expected_length % 2 !== 0) {
+        throw new Error('expected_length should be an even number.');
+      }
+      if (str.length !== expected_length) {
+        throw new Error('Expected length to be: ' + expected_length + '. ' +
+                        'But was: ' + str.length  + '. Remember a hex string is twice ' +
+                        'as long as the number of bytes desired.');
+      }
+      let bytes = [];
+      for (let i = 0; i < str.length; i += 2) {
+        if (!HEX_REGEX.test(str[i])) {
+          throw new Error('Invalid character \'' + str[i] + '\' at index ' + i);
+        }
+        if (!HEX_REGEX.test(str[i + 1])) {
+          throw new Error('Invalid character \'' + str[i + 1] + '\' at index ' + (i + 1));
+        }
+        bytes.push(parseInt(str.substr(i, 2), 16));
+      }
+      return bytes;
+    }
+
+    static _encodeNumber(num, expected_length) {
+      if (typeof num !== 'number') {
+        throw new Error('Input not a number: ' + num);
+      }
+      if (num < 0 || num > 65535) {
+        throw new Error('major and minor must be between 0 and 65535.');
+      }
+      let str;
+      // Convert number to string and pad with zeros
+      str = ('0000' + num.toString(16)).substr(-(MAJOR_MINOR_LENGTH*2));
+      // A hex string is twice as long as the byte array it represents.
+      let str_expected_length = expected_length * 2;
+      return IBeacon._encodeString(str, str_expected_length);
+    }
+
+    static _validateByteArray(arr, expected_length) {
+      if (arr.length !== expected_length) {
+        throw new Error('Expected length to be: ' + expected_length + '. ' +
+                        'But was: ' + arr.length + '.');
+      }
+      for (let i = 0; i < arr.length; i++) {
+        if (typeof arr[i] !== 'number') {
+          throw new Error('Unexpected value \'' + arr[i] + '\' at index ' + i + '.');
+        }
+        if (!(arr[i] >= 0x00 && arr[i] <= 0xFF)) {
+          throw new Error('Unexpected value \'' + arr[i] + '\' at index ' + i + '.');
+        }
+      }
+      return arr;
+    }
+  }
+  module.exports = IBeacon;
+})();
+
+},{"./eddystone-advertisement.js":1}],8:[function(require,module,exports){
 (() => {
   'use strict';
 
